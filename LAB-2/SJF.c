8240456ec71bfe84dc74c6a8cf8dcaf6fb37c6c3
tanmay-lang <tanmay.me23@bmsce.ac.in>
2025-03-12 12:42:38 +0530

    Add files via upload

---
+++ LAB-2/SJF.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+void sortByBurstTime(int processes[], int n, int bt[]) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (bt[j] > bt[j + 1]) {
+                int temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+
+                temp = processes[j];
+                processes[j] = processes[j + 1];
+                processes[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void findWaitingTime(int processes[], int n, int bt[], int wt[]) {
+    wt[0] = 0;
+    for (int i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];
+    }
+}
+
+void findTurnaroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+    }
+}
+
+void findAvgTime(int processes[], int n, int bt[]) {
+    int wt[n], tat[n];
+    float total_wt = 0, total_tat = 0;
+
+    sortByBurstTime(processes, n, bt);
+    findWaitingTime(processes, n, bt, wt);
+    findTurnaroundTime(processes, n, bt, wt, tat);
+
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("%d\t%d\t\t%d\t\t%d\n", processes[i], bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", total_wt / n);
+    printf("\nAverage Turnaround Time = %.2f\n", total_tat / n);
+}
+
+int main() {
+    int processes[] = {1, 2, 3, 4};
+    int n = sizeof processes / sizeof processes[0];
+    int burst_time[] = {5, 8, 12, 6};
+
+    findAvgTime(processes, n, burst_time);
+    return 0;
+}
